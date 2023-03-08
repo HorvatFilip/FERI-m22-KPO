b7c0dc04bd77da462ed9857cc80d1bb53f1e8d16
Filip <filip.horvat997@gmail.com>
2023-03-08 20:55:30 +0100

    modified:  - constructor of OrganismGroups  - Organism random moving
    modified:
     - time intervals not last longer (1h per 50ms -> 0.1h per 50ms)
     - moveToGoal() function to Organism class
    
    added:
     - SearchForFood and checkForPredators fucntionality
     - Added InteractionRange fucntion
    
    modified:
     - changed spawning point & feeding point for individual organisms
     - changed feeding/resting cycle
    
    modified:
     - searchForFood optimization
    
    modified:
     - running from predators
     - predator goalPos checking
    added:
     - gui display for species
    
    fixed:
     - loading data from gui
     - play/pause & reset simulation
    added:
     - time display
     - plants and plant logic
     - food counting
     - basic evolution
    modified:
     - path finding of organisms
    
    added:
     - avg values of orgSize, orgMaxVel and orgDetectRadius for each faze are now stored and display on chart
    modified:
     - how evolution changes values
    
    added:
     - energy -> when it runs out org goes back to homePos
    modified:
     - evolution now includes energy as well
    
    modified:
     - random movement
    
    modified:
     - spawning patterns
     - organisms now move at half the speed when out of energy
    
    added:
     - new chart: popSize
    modifed:
     - removed return to home signal when resting period started
     - added chart labels
    fixed:
     - infoPoints double maxVel shift()

--- vaja02/draw-component.js
+++ vaja02/draw-component.js
@@ -5,36 +5,18 @@ class DrawComponent {
         this.simCanvas.height = simCanvasConf.height;
         this.simCtx = this.simCanvas.getContext("2d");
 
-        this.infoCanvas = document.getElementById(infoCanvasConf.id);
-        this.infoCanvas.width = infoCanvasConf.width;
-        this.infoCanvas.height = infoCanvasConf.height;
-        this.infoCtx = this.infoCanvas.getContext("2d");
-        this.infoCtx.font = "10px Arial";
+        this.infoCtx = [];
         this.infoPoints = {};
-        this.timeStamp = 0;
+        this.chartLabels = ["Count", "Size", "MaxVelocity", "DetectRadius"];
         this.chartOffset = 20;
-    }
-    drawBoard() {
-        console.log("TODO - drawBoard");
     }
     clearSimCanvas() {
         this.simCtx.fillStyle = "rgb(50, 60, 70)";
         this.simCtx.fillRect(0, 0, this.simCanvas.width, this.simCanvas.height);
     }
-    clearChartCanvas() {
-        this.infoCtx.fillStyle = "rgb(50, 60, 70)";
-        this.infoCtx.fillRect(0, 0, this.infoCanvas.width, this.infoCanvas.height);
-    }
-    updateData(state) {
-        this.updateChartPoints(state.organismGroups);
-    }
     syncSimData(state) {
         this.clearSimCanvas();
         this.drawAllOrganisms(state.organismGroups);
-    }
-    syncInfoData(state) {
-        this.clearChartCanvas();
-        this.drawChart(state.timePassed);
     }
     drawAllOrganisms(organismGroups) {
         organismGroups.forEach(orgGroup => {
@@ -45,53 +27,101 @@ class DrawComponent {
     }
     drawOrganism(organism) {
         this.simCtx.beginPath();
-        this.simCtx.arc(organism.pos.x, organism.pos.y, organism.size, 0, Math.PI * 2);
+        this.simCtx.arc(organism.pos.x, organism.pos.y, organism.orgSize, 0, Math.PI * 2);
         this.simCtx.closePath();
-        this.simCtx.fillStyle = organism.color;
+        this.simCtx.fillStyle = organism.orgColor;
         this.simCtx.fill();
+    }
+
+    initChartPointVars() {
+        this.infoPoints = {};
+    }
+    createNewInfoCanvas(conf, val) {
+        const chartCanvas = document.createElement("canvas");
+        chartCanvas.setAttribute("id", val + "-info-canvas");
+        chartCanvas.width = conf.width;
+        chartCanvas.height = conf.height;
+        this.infoCanvasWidth = conf.width;
+        this.infoCanvasHeight = conf.height;
+        this.infoCtx.push(chartCanvas.getContext("2d"));
+        this.infoCtx[this.infoCtx.length - 1].fillStyle = "rgb(50, 60, 70)";
+        this.infoCtx[this.infoCtx.length - 1].fillRect(0, 0, this.infoCanvasWidth, this.infoCanvasHeight);
+        return chartCanvas;
+    }
+    clearChartCanvas() {
+        this.infoCtx.forEach(valCtx => {
+            valCtx.fillStyle = "rgb(50, 60, 70)";
+            valCtx.fillRect(0, 0, this.infoCanvasWidth, this.infoCanvasHeight);
+        });
+        this.drawChartAxis();
+    }
+    syncInfoData(state) {
+        this.clearChartCanvas();
+        this.updateChartPoints(state.organismGroups);
+        this.drawChart(state.timePassed);
+    }
+    updateData(state) {
+        this.updateChartPoints(state.organismGroups);
     }
     updateChartPoints(organismGroups) {
         organismGroups.forEach(orgGroup => {
-            if (this.infoPoints[orgGroup.type] === undefined) {
-                this.infoPoints[orgGroup.type] = {
-                    orgColor: orgGroup.orgColor,
-                    popSize: [orgGroup.popSize]
+            let values = orgGroup.getChartValues();
+            if (this.infoPoints[orgGroup.id] === undefined) {
+                this.infoPoints[orgGroup.id] = {
+                    orgColor: orgGroup.conf.orgColor,
+                    0: [values[0]],
+                    1: [values[1]],
+                    2: [values[2]],
+                    3: [values[3]]
                 };
             } else {
-                if (this.infoPoints[orgGroup.type]["orgColor"] != orgGroup.orgColor) {
-                    this.infoPoints[orgGroup.type]["orgColor"] = orgGroup.orgColor;
-                }
-                this.infoPoints[orgGroup.type]["popSize"].push(orgGroup.popSize);
-                if (this.infoPoints[orgGroup.type]["popSize"].length > this.infoCanvas.width - this.chartOffset) {
-                    this.infoPoints[orgGroup.type]["popSize"].shift();
+                this.infoPoints[orgGroup.id][0].push(values[0]);
+                this.infoPoints[orgGroup.id][1].push(values[1] * 20);
+                this.infoPoints[orgGroup.id][2].push(values[2] * 20);
+                this.infoPoints[orgGroup.id][3].push(values[3]);
+                if (this.infoPoints[orgGroup.id][0].length * 24 > this.infoCanvasWidth - this.chartOffset) {
+                    this.infoPoints[orgGroup.id][0].shift();
+                    this.infoPoints[orgGroup.id][1].shift();
+                    this.infoPoints[orgGroup.id][2].shift();
+                    this.infoPoints[orgGroup.id][3].shift();
                 }
             }
         });
     }
-    drawChart() {
-        this.infoCtx.strokeStyle = "black";
-        this.infoCtx.beginPath();
-        this.infoCtx.moveTo(this.chartOffset, this.infoCanvas.height - this.chartOffset);
-        this.infoCtx.lineTo(this.chartOffset, 0);
-
-        this.infoCtx.moveTo(this.chartOffset, this.infoCanvas.height - this.chartOffset);
-        this.infoCtx.lineTo(this.infoCanvas.width, this.infoCanvas.height - this.chartOffset);
-        this.infoCtx.stroke();
-
-        let organismTypes = Object.keys(this.infoPoints);
-        organismTypes.forEach(orgType => {
-            this.infoCtx.strokeStyle = this.infoPoints[orgType]["orgColor"];
-            let timeInterval = 0;
-            this.infoCtx.beginPath();
-            for (let i = 0; i < this.infoPoints[orgType]["popSize"].length - 1; i++) {
-                this.infoCtx.moveTo(timeInterval + this.chartOffset, this.infoCanvas.height - this.chartOffset - this.infoPoints[orgType]["popSize"][i]);
-                timeInterval += 24;
-                this.infoCtx.lineTo(timeInterval + this.chartOffset, this.infoCanvas.height - this.chartOffset - this.infoPoints[orgType]["popSize"][i + 1]);
-                if (timeInterval + this.chartOffset > this.infoCanvas.width - this.chartOffset) {
-                    this.infoPoints[orgType]["popSize"].shift();
-                }
+    drawChartAxis() {
+        let labelIndx = 0;
+        this.infoCtx.forEach(valCtx => {
+            valCtx.strokeStyle = "#212529";
+            valCtx.beginPath();
+            valCtx.moveTo(this.chartOffset, this.infoCanvasHeight - this.chartOffset);
+            valCtx.lineTo(this.chartOffset, 0);
+            valCtx.moveTo(this.chartOffset, this.infoCanvasHeight - this.chartOffset);
+            valCtx.lineTo(this.infoCanvasWidth, this.infoCanvasHeight - this.chartOffset);
+            valCtx.stroke();
+            valCtx.font = "35px Comic Sans MS";
+            valCtx.fillStyle = "#212529";
+            valCtx.fillText(this.chartLabels[labelIndx++], this.chartOffset + 20, this.chartOffset + 35)
+        });
+    }
+    drawChart(intervalType) {
+        let timeInterval = 0;
+        Object.keys(this.infoPoints).forEach(id => {
+            if (this.infoPoints[id][0].length < 2) {
+                return;
             }
-            this.infoCtx.stroke();
+            for (let j = 0; j < 4; j++) {
+                this.infoCtx[j].strokeStyle = this.infoPoints[id].orgColor;
+                timeInterval = 0;
+                this.infoCtx[j].strokeStyle = this.infoPoints[id].orgColor;
+                this.infoCtx[j].beginPath();
+                for (let i = 0; i < this.infoPoints[id][j].length - 1; i++) {
+                    this.infoCtx[j].moveTo(timeInterval + this.chartOffset, this.infoCanvasHeight - this.chartOffset - this.infoPoints[id][j][i]);
+                    timeInterval += 24;
+                    this.infoCtx[j].lineTo(timeInterval + this.chartOffset, this.infoCanvasHeight - this.chartOffset - this.infoPoints[id][j][i + 1]);
+                }
+                this.infoCtx[j].stroke();
+
+            }
         });
     }
 }
--- vaja02/island.js
+++ vaja02/island.js
@@ -2,81 +2,236 @@ class Organism {
     constructor(orgNewPos) {
         Object.assign(this, orgNewPos);
     }
+    setGoalPos(newGoalPos) {
+        this.goalPos = new Vector(newGoalPos.x, newGoalPos.y);
+    }
+    moveToSpawnPoint() {
+        this.goalPos = this.homePos;
+    }
     updatePosition(state) {
-        let rndMove = true;
-        if (this.pos.x >= state.display.simCanvas.width - 30 || this.pos.x <= 30) {
-            this.velocity = new Vector(this.velocity.x * (-1), this.velocity.y)
-            rndMove = false;
-        }
-        if (this.pos.y >= state.display.simCanvas.height - 30 || this.pos.y <= 30) {
-            this.velocity = new Vector(this.velocity.x, this.velocity.y * (-1));
-            rndMove = false;
-        }
-        if (rndMove) {
-            let changeVelocity = Math.floor(Math.random() * 4);
-            switch (changeVelocity) {
-                case 0:
-                    this.velocity = new Vector(0, 0);
-                    break;
-                case 1:
-                    this.velocity = new Vector(this.velocity.x * (-1), this.velocity.y);
-                    break;
-                case 2:
-                    this.velocity = new Vector(this.velocity.x, this.velocity.y * (-1));
-                    break;
-                case 3:
-                    this.velocity = new Vector(this.velocity.x * (-1), this.velocity.y * (-1));
-                    break;
+        if (this.type !== "plant") {
+            if (this.stage == "feeding") {
+                this.trueEnergy = this.trueEnergy - this.orgSize - this.maxVelocity - this.detectRadius;
+            }
+            if (this.checkBounds(state) == 0) {
+                return new Organism({
+                    ...this,
+                    pos: this.pos.add(this.velocity)
+                });
+            } else {
+                if (this.goalPos == null || this.pos.equals(this.goalPos)) {
+                    this.goalPos = null;
+                    this.makeRandomMove();
+                } else if (this.goalPos != null) {
+                    this.makeMoveToGoal();
+                }
+                return new Organism({
+                    ...this,
+                    pos: this.pos.add(this.velocity)
+                });
             }
         }
+        return this;
 
+    }
+    getNormalPoint(p, a, b) {
+        let ap = p.subtract(a);
+        let ab = b.subtract(a);
+        ab = ab.normalize();
+        ab = ab.multiply(ap.dotProduct(ab));
+        return a.add(ab);
+    }
+    followPath(path) {
+        let predict = this.velocity.get();
+        predict = predict.normalize();
+        predict = predict.multiply(25);
+        let predictLoc = this.pos.add(predict);
+        let bestDistance = 10000;
+        let newTarget = null;
+        for (let i = 0; i < path.length - 1; i++) {
+            let a = path[i];
+            let b = path[i + 1];
+            let normalPoint = this.getNormalPoint(predictLoc, a, b);
+            if (normalPoint.x < a.x || normalPoint.x > b.x) {
+                normalPoint = b.get();
+            }
+            let distance = predictLoc.distance(normalPoint);
+            if (distance < bestDistance) {
+                bestDistance = distance;
+                newTarget = normalPoint;
+            }
+        }
+        this.setGoalPos(newTarget);
+    }
+    checkBounds(state) {
+        let moveScenario = 1;
+        if (this.pos.x >= state.display.simCanvas.width - 50 || this.pos.x <= 50) {
+            this.velocity = new Vector(this.velocity.x * (-1), this.velocity.y)
+            this.goalPos = null;
+            moveScenario = 0;
+        }
+        if (this.pos.y >= state.display.simCanvas.height - 50 || this.pos.y <= 50) {
+            this.velocity = new Vector(this.velocity.x, this.velocity.y * (-1));
+            this.goalPos = null;
+            moveScenario = 0;
+        }
+        return moveScenario;
+    }
+    makeMoveToGoal() {
+        let maxVel = this.maxVelocity;
+        if (this.trueEnergy < 1) {
+            maxVel = maxVel / 2;
+        }
 
-        return new Organism({
-            ...this,
-            pos: this.pos.add(this.velocity)
-        });
+        let x = (this.goalPos.x - this.pos.x) / maxVel;
+        if (x >= 1) {
+            x = maxVel;
+        } else if (x <= -1) {
+            x = -maxVel;
+        } else {
+            x = this.goalPos.x - this.pos.x;
+        }
+        let y = (this.goalPos.y - this.pos.y) / maxVel;
+        if (y >= 1) {
+            y = maxVel;
+        } else if (y <= -1) {
+            y = -maxVel;
+        } else {
+            y = this.goalPos.y - this.pos.y;
+        }
+        this.velocity = new Vector(x, y);
+    }
+    makeRandomMove() {
+        let theta = Math.random() * 2 * Math.PI;
+        let maxVel = this.maxVelocity;
+        if (this.trueEnergy < 1) {
+            maxVel = maxVel / 2;
+        }
+        let vel = new Vector(maxVel, maxVel);
+        let center = this.pos.add(vel);
+        let x = center.x + 50 * Math.cos(theta);
+        let y = center.y + 50 * Math.sin(theta);
+        this.setGoalPos({ x: x, y: y });
+    }
+    inRangeOfInteraction(org2) {
+        let d = Math.sqrt(
+            Math.pow(this.pos.x - org2.pos.x, 2) + Math.pow(this.pos.y - org2.pos.y, 2)
+        );
+        let canEatOrg2 = this.canEat(org2);
+        let canBeEaten = org2.canEat(this);
+        if (d <= this.orgSize && canEatOrg2) {
+            this.eatenFood++;
+            //if (this.eatenFood > 1) {
+            //    this.setGoalPos(this.homePos);
+            //}
+            return 1;
+        } else if (d <= this.detectRadius) {
+            if (canEatOrg2) {
+                return 2;
+            } else if (canBeEaten) {
+                return 3;
+            }
+        } else {
+            return 4;
+        }
+    }
+    canEat(org2) {
+        if (this.eatingSize >= org2.orgSize) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 class OrganismGroup {
-    constructor(type, orgColor, initialPopSize, orgSize, maxVelocity, viewRange, spawnPoint, spawnVelocity = new Vector(0, 0)) {
-        this.type = type;
-        this.orgColor = orgColor;
-        this.initialPopSize = initialPopSize;
-        this.orgSize = orgSize;
-        this.orgMaxVelocity = maxVelocity;
-        this.orgViewRange = viewRange;
-
-        this.spawnPoint = spawnPoint;
-        this.spawnVelocity = spawnVelocity;
+    constructor(id, conf) {
+        /*
+        this.id = conf.id;
+        this.type = conf.type;
+        this.orgColor = conf.orgColor;
+        this.orgSize = conf.orgSize;
+        this.orgMaxVelocity = conf.orgMaxVelocity;
+        this.detectRadius = conf.detectRadius;
+        this.diet = conf.diet;
+        this.homePos = conf.homePos;
+        this.feedingPos = conf.feedingPos;
+        this.population = [];
         this.popSize = 0;
+        this.initialPopSize = conf.initialPopSize;
+        this.addOrganisms(this.initialPopSize);
+        */
+        this.id = id;
+        this.conf = conf;
         this.population = [];
-        this.addOrganisms(initialPopSize);
+        this.popSize = 0;
+        this.popId = 0;
+        this.addOrganisms(this.conf.initialPopSize);
+        this.outsidePath = [];
     }
-    changeSpecifications(config) {
-        this.type = config["type"];
-        this.rp = config["rp"];
-        this.sp = config["sp"];
-        this.k = config["k"];
-        this.popSize = config["popSize"];
-        this.orgSize = config["orgSize"];
-        this.orgColor = config["orgColor"];
-        this.spawnPoint = new Vector(config["spawnPoint"][0], config["spawnPoint"][1]);
+    createOutsidePath(display) {
+        let point01 = new Vector(100, 100);
+        let point02 = new Vector(1500 - 100, 100);
+        let point03 = new Vector(100, 1500 - 100);
+        let point04 = new Vector(1500 - 100, 1500 - 100);
+        let point05 = new Vector(100, 100);
+        this.outsidePath = [point01, point02, point03, point04, point05];
+    }
+    changeConfiguration(newConf) {
+        this.conf = Object.assign(this.conf, newConf);
         this.population = [];
-        this.addOrganisms(this.popSize);
+        this.popSize = 0;
+        this.addOrganisms(this.conf.initialPopSize);
+    }
+    getRandomPointInsideCircle(R, center) {
+        let r = R * Math.sqrt(Math.random());
+        let theta = Math.random() * 2 * Math.PI;
+        let x = center.x + r * Math.cos(theta);
+        let y = center.y + r * Math.sin(theta);
+        return new Vector(x, y);
+    }
+    getRandomPointOnCircle(R, center) {
+        let theta = Math.random() * 2 * Math.PI;
+        let x = center.x + R * Math.cos(theta);
+        let y = center.y + R * Math.sin(theta);
+        return new Vector(x, y);
     }
     addOrganisms(count) {
+        let homePos;
+        let vel;
+        let rndSize;
         for (let i = 0; i < count; i++) {
             this.popSize++;
+            this.popId++;
+            if (this.conf.type == "plant") {
+                homePos = this.getRandomPointInsideCircle(this.conf.feedingZoneRadius, this.conf.homePos);
+            } else {
+                homePos = this.getRandomPointOnCircle(this.conf.feedingZoneRadius, this.conf.feedingPos);
+            }
+            vel = new Vector(
+                Math.random() * this.conf.orgMaxVelocity * 2 - this.conf.orgMaxVelocity,
+                Math.random() * this.conf.orgMaxVelocity * 2 - this.conf.orgMaxVelocity
+            )
+            rndSize = this.conf.orgSize * 0.7;
+            rndSize = Math.random() * (this.conf.orgSize - rndSize) + rndSize;
             const orgStats =
             {
-                type: this.type,
-                color: this.orgColor,
-                id: this.type + "-" + this.popSize,
-                pos: this.spawnPoint,
-                size: this.orgSize,
-                maxVelocity: this.orgMaxVelocity,
-                velocity: this.spawnVelocity
+                id: this.conf.type + "-" + this.popId,
+                type: this.conf.type,
+                orgColor: this.conf.orgColor,
+                orgSize: rndSize,
+                eatingSize: this.conf.orgSize * 0.8,
+                maxVelocity: this.conf.orgMaxVelocity,
+                detectRadius: this.conf.detectRadius,
+                baseEnergy: this.conf.baseEnergy,
+                trueEnergy: this.conf.baseEnergy,
+                stage: "resting",
+                diet: this.conf.diet,
+                eatenFood: 0,
+                velocity: vel,
+                homePos: homePos,
+                pos: homePos,
+                goalPos: null
             }
             this.population.push(
                 new Organism(orgStats)
@@ -87,38 +242,156 @@ class OrganismGroup {
         this.population.splice(0, count);
         this.popSize -= count;
     }
-    updatePopulation() {
-        let populationChange = 0;
-        for (let i = 0; i < this.popSize; i++) {
-            if (Math.random() < this.rp) {
-                populationChange++;
-            }
-            let deathChance = Math.random();
-            if (deathChance < this.sp || deathChance < (this.k * this.popSize)) {
-                populationChange--;
-            }
-        }
-        if (populationChange > 0) {
-            this.addOrganisms(populationChange);
-        }
-        else {
-            this.removeOrganisms(Math.abs(populationChange));
-        }
+    removeById(id) {
+        this.population = this.population.filter(org => org.id != id);
+        this.popSize--;
     }
     updatePosition(state) {
         this.population = this.population.map(organism => {
-            return organism.updatePosition(state);
+            return organism.updatePosition(state, this.outsidePath);
         });
         return this;
+    }
+    moveToFeedingZone(resetFood = false) {
+        this.population.forEach(org => {
+            if (resetFood) {
+                org.eatenFood = 0;
+                org.trueEnergy = org.baseEnergy;
+                org.stage = "feeding";
+                //let feedingPos = this.getRandomPointInsideCircle(this.conf.feedingZoneRadius, this.conf.feedingPos);
+                //org.setGoalPos(feedingPos);
+            } else if (org.eatenFood < 2) {
+                //let feedingPos = this.getRandomPointInsideCircle(this.conf.feedingZoneRadius, this.conf.feedingPos);
+                //org.setGoalPos(feedingPos);
+            }
+        });
+    }
+    changeStage(stage) {
+        if (this.conf.type != "plant") {
+            if (stage == "feeding") {
+                this.moveToFeedingZone(true);
+            } else if (stage == "resting") {
+                this.population.forEach(org => {
+                    if (org.eatenFood < 1) {
+                        this.removeById(org.id);
+                    } else {
+                        org.stage = "resting";
+                        if (org.eatenFood > 1) {
+                            if (this.conf.type == "insect") {
+                                this.spawnChild(org, 2);
+                            } else {
+                                this.spawnChild(org, 1);
+                            }
+                        }
+                        //let homePos = this.getRandomPointOnCircle(this.conf.feedingZoneRadius, this.conf.feedingPos);
+                        //org.setGoalPos(homePos);
+                    }
+                });
+            }
+        }
+    }
+    isAlive(org) {
+        this.population.forEach(o => {
+            if (o.id == org.id) {
+                return true;
+            }
+        });
+        return false;
+    }
+    spawnChild(org, count) {
+        let homePos = null;
+        let orgSize = org.orgSize + Math.random() * 1.5 - 0.6;
+        let maxVelocity = org.maxVelocity + Math.random() * 1.1 - 0.4;
+        let detectRadius = org.detectRadius + Math.random() * 2 - 0.8;
+        let baseEnergy = org.baseEnergy + Math.random() * 1.5 - 0.5;
+        if (orgSize < 0) {
+            orgSize = 0.2;
+        }
+        if (maxVelocity < 0) {
+            maxVelocity = 0.2;
+        }
+        if (detectRadius < 0) {
+            detectRadius = 0.2;
+        }
+
+        this.popSize += count;
+
+        for (let i = 0; i < count; i++) {
+            this.popId++;
+            homePos = this.getRandomPointOnCircle(this.conf.feedingZoneRadius, this.conf.feedingPos);
+            const orgStats =
+            {
+                id: this.conf.type + "-" + this.popId,
+                type: this.conf.type,
+                orgColor: this.conf.orgColor,
+                orgSize: orgSize,
+                eatingSize: orgSize * 0.8,
+                maxVelocity: maxVelocity,
+                detectRadius: detectRadius,
+                baseEnergy: baseEnergy,
+                trueEnergy: baseEnergy,
+                stage: "resting",
+                diet: this.conf.diet,
+                eatenFood: 0,
+                velocity: org.velocity,
+                homePos: homePos,
+                pos: org.pos,
+                goalPos: homePos
+            }
+            this.population.push(
+                new Organism(orgStats)
+            );
+        }
+    }
+    respawn() {
+        this.population = [];
+        this.popSize = 0;
+        this.addOrganisms(this.conf.initialPopSize);
+        return this;
+    }
+    getChartValues() {
+        let avgOrgSize = 0;
+        let avgMaxVel = 0;
+        let avgDetect = 0;
+        this.population.forEach(org => {
+            avgOrgSize += org.orgSize;
+            avgMaxVel += org.maxVelocity;
+            avgDetect += org.detectRadius;
+        });
+        return [this.popSize, avgOrgSize / this.popSize, avgMaxVel / this.popSize, avgDetect / this.popSize];
     }
 }
 
 class DateTimeTracker {
     constructor(startDate = { year: 0, month: 0, day: 0, hour: 0 }) {
+        this.startDate = startDate;
         this.date = startDate;
+        this.interval = null;
+        this.run = true;
+        this.timeSpeed = 40;
+    }
+    setTimeSpeed(timeSpeed) {
+        this.timeSpeed = 70 - timeSpeed * 10;
+        clearInterval(this.interval);
         this.interval = setInterval(() => {
-            this.addHours(1);
-        }, 50);
+            if (this.run) {
+                this.addHours(0.05);
+            }
+        }, this.timeSpeed);
+    }
+    resetDate() {
+        if (this.interval !== null) {
+            clearInterval(this.interval);
+        }
+        this.date = { year: 0, month: 0, day: 0, hour: 0 };
+        this.interval = setInterval(() => {
+            if (this.run) {
+                this.addHours(0.05);
+            }
+        }, this.timeSpeed);
+    }
+    toggleTimePassage(run) {
+        this.run = run;
     }
     addYears(count) {
         this.date["year"] += count;
@@ -173,18 +446,31 @@ class EcoSystem {
         this.dateTime = dateTimeTracker;
         this.organismGroups = [];
     }
-    addOrganismGroup(organismGroup) {
-        this.organismGroups.push(organismGroup);
+    addOrganismGroup(orgGroupConf) {
+        let typeCount = 0;
+        this.organismGroups.forEach(orgGroup => {
+            if (orgGroup.conf.type == orgGroupConf.type) {
+                typeCount++;
+            }
+        });
+        const orgGroupId = orgGroupConf.type + "-" + typeCount;
+        const newOrgGroup = new OrganismGroup(orgGroupId, orgGroupConf);
+        newOrgGroup.createOutsidePath();
+        this.organismGroups.push(newOrgGroup);
     }
-    changeOrganismGroupSpecs(type, config) {
+    changeOrganismGroupConfiguration(id, newConfig) {
         for (let i = 0; i < this.organismGroups.length; i++) {
-            if (this.organismGroups[i].type == type) {
-                this.organismGroups[i].changeSpecifications(config);
+            if (this.organismGroups[i].id == id) {
+                this.organismGroups[i].changeConfiguration(newConfig);
+                break;
             }
         }
     }
-    removeOrganismGroupByType(type) {
-        this.organismGroups = this.organismGroups.organismGroups.filter(orgGroup => orgGroup.type != type);
+    removeOrganismGroupById(id) {
+        this.organismGroups = this.organismGroups.filter(orgGroup => orgGroup.id != id);
+    }
+    changeTimePassingSpeed(timeSpeed) {
+        this.dateTime.setTimeSpeed(timeSpeed);
     }
 
 }
